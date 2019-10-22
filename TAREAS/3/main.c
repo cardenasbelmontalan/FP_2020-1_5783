e6b69a506551549419d2fa6ae038620cf34a142d
cardenasbelmontalan <alanbelmont_5@outlook.com>
2019-10-22 18:48:03 -0500

    Correcion

--- TAREAS/3/main.c
+++ TAREAS/3/main.c
@@ -1,18 +1,15 @@
-#include <stdlib.h>
 #include <stdio.h>
-int main(int argc, char*argv[]){
+#include <stdlib.h>
+int main(int argc, char const *argv[])
+{
 	double x, y;
-
-	x= atof(argv[1]);
-	y= atof(argv[2]);
-
-	printf("La suma es %g\n", x+y);
-
-	printf("La resta es %g\n", x-y);
-
-	printf("La multiplicación es %g\n", x*y);
-
-	printf("La división es %.21f\n", x/y);
-
+	x=atof(argv[1]);
+	y=atof(argv[2]);
+	
+	printf("%.2lf\n",x+y );
+	printf("%.2lf\n",x-y );
+	printf("%.2lf\n",x*y );
+	printf("%.2lf\n",x/y );
+	
 	return 0;
 }
